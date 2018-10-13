2d37d9937321815ccb04a1fa33384f474652fe7d
Fatoumata Balde <Fatoumata2004code@gmail.com>
2018-10-13 15:44:16 -0400

    Update data.js
    have to finish the undefined value one

--- data.js
+++ data.js
@@ -2,4 +2,27 @@
 let message = "No Regrets!";
 console.log(message);
 
+let something = "Happy place";
+console.log(something);
+
+let Doesnotlikerollercoaster = false;
+console.log(Doesnotlikerollercoaster);
+
+let NICE= true;
+console.log(NICE);
+
+let variable = 3+1 
+console.log(variable);
+
+let somenumber = 5
+console.log(somenumber);
+
+let whatever = 9
+console.log(whatever)
+
+let word= "It's my life"
+console.log(word)
+
+
+
 // Type your code below!
